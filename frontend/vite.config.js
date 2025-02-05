6e82f7c40f59bfad1e147d717cd69f73721f5fca
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-02-05 21:19:47 +0100

    setup Tailwind CSS for React frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080"
-      }
-    }
-  }
-})
+	plugins: [react(), tailwindcss(), svgr()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:8080',
+			},
+		},
+	},
+});
