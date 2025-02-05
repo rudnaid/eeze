2943f6c1057fa5ab725cb6987327cb89085371a8
rudnaid <david.rudnai@gmail.com>
2025-02-05 12:09:49 +0100

    init backend

---
+++ backend/src/main/java/com/codecool/spendeeze/SpendeezeApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.spendeeze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendeezeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpendeezeApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/codecool/spendeeze/SpendeezeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.spendeeze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendeezeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
