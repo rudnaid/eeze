df527d598ca1cad35a7fabac06f04577af65baed
Vica1921 <166558654+Vica1921@users.noreply.github.com>
2025-02-18 22:01:47 +0100

    refactor(income): modify IncomeService to work with Spring Data JPA repository interface

--- backend/src/main/java/com/codecool/spendeeze/service/IncomeService.java
+++ backend/src/main/java/com/codecool/spendeeze/service/IncomeService.java
@@ -1,54 +1,66 @@
 package com.codecool.spendeeze.service;
 
-import com.codecool.spendeeze.model.Income;
+import com.codecool.spendeeze.model.entity.Income;
 import com.codecool.spendeeze.model.IncomeDTO;
+import com.codecool.spendeeze.model.entity.User;
 import com.codecool.spendeeze.repository.IncomeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.UUID;
 
 @Service
 public class IncomeService {
     private final IncomeRepository incomeRepository;
+    //TODO implement UserRepository
+    private final UserRepository userRepository;
 
-    public IncomeService(IncomeRepository incomeRepository) {
+    public IncomeService(IncomeRepository incomeRepository, UserRepository userRepository) {
         this.incomeRepository = incomeRepository;
+        this.userRepository = userRepository;
     }
 
-    public int addIncome(IncomeDTO income, int userId) {
-        return incomeRepository.addIncome(income, userId);
+    public Income addIncome(IncomeDTO income, UUID userId) {
+        User user = userRepository.findUserByPublicId(userId);
+        Income incomeEntity = convertDtoToEntity(income);
+        incomeEntity.setUser(user);
+        return incomeRepository.save(incomeEntity);
     }
 
-    public List<IncomeDTO> getAllIncomesByUser(int userId) {
-        List<Income> incomes = incomeRepository.getAllIncomesByUser(userId);
+    private Income convertDtoToEntity(IncomeDTO incomeDto) {
+        Income incomeEntity = new Income();
+        incomeEntity.setAmount(incomeDto.amount());
+        incomeEntity.setDate(incomeDto.date());
+        return incomeEntity;
+    }
+
+    public List<IncomeDTO> findIncomesByUserId(UUID userId) {
+        User user = userRepository.findUserByPublicId(userId);
+        List<Income> incomes = incomeRepository.findIncomesByUser(user);
         List<IncomeDTO> incomeDTOs = new ArrayList<>();
         for (Income income : incomes) {
-            incomeDTOs.add(new IncomeDTO(income.getAmount(), income.getDate()));
+            incomeDTOs.add(new IncomeDTO(income.getPublicId(), income.getAmount(), income.getDate()));
         }
         return incomeDTOs;
     }
 
-    public IncomeDTO getIncomeById(int incomeId) {
-        Income income = incomeRepository.getIncomeById(incomeId).orElseThrow(() -> new NoSuchElementException("Income with id " + incomeId + " not found"));
-        return new IncomeDTO(income.getAmount(), income.getDate());
+    public IncomeDTO findIncomeById(UUID incomeId) {
+        Income income = incomeRepository.findIncomeByPublicId(incomeId).orElseThrow(() -> new NoSuchElementException("Income with id " + incomeId + " not found"));
+        return new IncomeDTO(income.getPublicId(), income.getAmount(), income.getDate());
     }
 
-    public int updateIncome(int incomeId, IncomeDTO income) {
-        int numberOfUpdatedEntries = incomeRepository.updateIncome(incomeId, income);
-        if (numberOfUpdatedEntries > 0) {
-            return numberOfUpdatedEntries;
-        } else {
-            throw new NoSuchElementException("Income with id " + incomeId + " not found");
-        }
+    public IncomeDTO updateIncome(IncomeDTO incomeDTO) {
+        Income income = incomeRepository.findIncomeByPublicId(incomeDTO.publicId()).orElseThrow(() -> new NoSuchElementException("Income with id " + incomeDTO.publicId() + " not found"));
+        income.setAmount(incomeDTO.amount());
+        income.setDate(incomeDTO.date());
+        incomeRepository.save(income);
+        return new IncomeDTO(income.getPublicId(), income.getAmount(), income.getDate());
     }
 
-    public int deleteIncome(int incomeId) {
-        int idOfDeletedIncome = incomeRepository.deleteIncome(incomeId);
-        if (idOfDeletedIncome == -1) {
-            throw new NoSuchElementException("Income with id " + incomeId + " not found");
-        }
-        return idOfDeletedIncome;
+    public void deleteIncome(UUID incomeId) {
+        Income income = incomeRepository.findIncomeByPublicId(incomeId).orElseThrow(() -> new NoSuchElementException("Income with id " + incomeId + " not found"));
+        incomeRepository.delete(income);
     }
 }
