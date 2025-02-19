bd281b143f43ad1f671377301b98edc60809e965
rudnaid <david.rudnai@gmail.com>
2025-02-19 11:30:17 +0100

    fix(expanse): add missing UUID publicId to ExpenseResponseDTO

--- backend/src/main/java/com/codecool/spendeeze/model/dto/ExpenseResponseDTO.java
+++ backend/src/main/java/com/codecool/spendeeze/model/dto/ExpenseResponseDTO.java
@@ -1,6 +1,7 @@
 package com.codecool.spendeeze.model.dto;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
-public record ExpenseResponseDTO(double amount, LocalDate transactionDate, String expenseCategory) {
+public record ExpenseResponseDTO(UUID publicId, double amount, LocalDate transactionDate, String expenseCategory) {
 }
