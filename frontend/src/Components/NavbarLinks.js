fa12891078fec77bd92bd1065772bb3b6833ac97
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-03-02 23:26:14 +0100

    move ROUTES to a js file and import them to the Navbar.jsx file

---
+++ frontend/src/Components/NavbarLinks.js
@@ -0,0 +1,6 @@
+export const ROUTES = [
+    { name: "Home", path: "/home" },
+    { name: "Balance", path: "/" },
+    { name: "Income", path: "/income" },
+    { name: "Expenses", path: "/expenses" },
+  ];
