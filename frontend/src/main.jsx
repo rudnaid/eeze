08d5a962b2cd10b87c8a075bc93783e708ad6440
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-03-05 00:47:24 +0100

    change layout

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css';
-import Layout from './Pages/Layout.jsx';
 import { Expenses } from './Pages/Expenses.jsx';
 import Income from './Pages/Income.jsx';
 import IncomeCreator from './Pages/IncomeCreator.jsx';
@@ -11,42 +10,54 @@ import ExpenseCreator from './Pages/ExpenseCreator.jsx';
 import Homepage from './Pages/Homepage.jsx';
 import Login from './Pages/Login.jsx';
 import Register from './Pages/Register.jsx';
+import PublicLayout from './Layouts/PublicLayout.jsx';
+import AuthenticatedLayout from './Layouts/AuthenticatedLayout.jsx';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <Layout />,
+		element: <PublicLayout />, 
 		children: [
 			{
-				path: '/expenses',
-				element: <Expenses />,
+				index: true,
+				element: <Homepage />,
 			},
 			{
-				path: '/expenses/create',
-				element: <ExpenseCreator />,
+				path: 'login',
+				element: <Login />,
 			},
 			{
-				path: '/income',
-				element: <Income />
-			},
-			{
-				path: '/income/create',
-				element: <IncomeCreator />
+				path: 'register',
+				element: <Register />,
 			},
 		],
 	},
+
 	{
 		path: '/home',
-		element: <Homepage />
-
-	},
-	{
-		path: '/login',
-		element: <Login />,
-	},
-	{
-		path: '/register',
-		element: <Register />,
+		element: <AuthenticatedLayout />, 
+		children: [
+			{
+				index: true,
+				element: <Expenses />,
+			},
+			{
+				path: 'expenses',
+				element: <Expenses />,
+			},
+			{
+				path: 'expenses/create',
+				element: <ExpenseCreator />,
+			},
+			{
+				path: 'income',
+				element: <Income />,
+			},
+			{
+				path: 'income/create',
+				element: <IncomeCreator />,
+			},
+		],
 	},
 ]);
 
