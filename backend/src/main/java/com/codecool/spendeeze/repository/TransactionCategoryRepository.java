b144b824996f826645ca05fe8625823bdeef813e
Vica1921 <166558654+Vica1921@users.noreply.github.com>
2025-03-04 23:42:23 +0100

    fix(transactionCategory): Remove duplicated method from TransactionCategoryRepository and add Autowired annotation for controller and service classes

--- backend/src/main/java/com/codecool/spendeeze/repository/TransactionCategoryRepository.java
+++ backend/src/main/java/com/codecool/spendeeze/repository/TransactionCategoryRepository.java
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TransactionCategoryRepository extends JpaRepository<TransactionCategory, Long> {
     TransactionCategory getTransactionCategoryByName(String name);
-    TransactionCategory findByName(String category);
 }
