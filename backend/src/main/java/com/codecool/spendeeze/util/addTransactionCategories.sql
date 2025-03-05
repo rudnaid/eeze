e52cc3e3d780e88ebb9319a2c7fea032ec85b7ea
rudnaid <david.rudnai@gmail.com>
2025-03-05 13:38:45 +0100

    refactor(database): remove deprecated dummy data generators

--- backend/src/main/java/com/codecool/spendeeze/util/addTransactionCategories.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO transaction_category (name) VALUES ('groceries');
-INSERT INTO transaction_category (name) VALUES ('reoccurring');
-INSERT INTO transaction_category (name) VALUES ('leisure');
-INSERT INTO transaction_category (name) VALUES ('transportation');
-INSERT INTO transaction_category (name) VALUES ('clothing');
-INSERT INTO transaction_category (name) VALUES ('medical');
