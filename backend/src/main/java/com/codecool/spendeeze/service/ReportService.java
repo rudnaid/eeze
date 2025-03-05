0cf62b9b7f4ca41d1c494e9e600c22a471a48c48
rudnaid <david.rudnai@gmail.com>
2025-03-05 09:40:39 +0100

    feat(reports): add method getMonthlyReport to serve monthly expense report for selected month

--- backend/src/main/java/com/codecool/spendeeze/service/ReportService.java
+++ backend/src/main/java/com/codecool/spendeeze/service/ReportService.java
@@ -2,11 +2,15 @@ package com.codecool.spendeeze.service;
 
 import com.codecool.spendeeze.model.dto.ReportDTO;
 import com.codecool.spendeeze.model.dto.TotalExpenseByTransactionCategoryDTO;
+import com.codecool.spendeeze.model.dto.reports.CategoryReport;
+import com.codecool.spendeeze.model.entity.Member;
 import com.codecool.spendeeze.repository.ExpenseRepository;
 import com.codecool.spendeeze.repository.IncomeRepository;
+import com.codecool.spendeeze.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 
@@ -15,10 +19,12 @@ public class ReportService {
 
     private final IncomeRepository incomeRepository;
     private final ExpenseRepository expenseRepository;
+    private final MemberRepository memberRepository;
 
-    public ReportService(IncomeRepository incomeRepository, ExpenseRepository expenseRepository) {
+    public ReportService(IncomeRepository incomeRepository, ExpenseRepository expenseRepository, MemberService memberService, MemberRepository memberRepository) {
         this.incomeRepository = incomeRepository;
         this.expenseRepository = expenseRepository;
+        this.memberRepository = memberRepository;
     }
 
     public ReportDTO generateReport(UUID memberPublicId) {
@@ -33,5 +39,13 @@ public class ReportService {
 
     public List<TotalExpenseByTransactionCategoryDTO> getTotalExpenseByTransactionCategory(UUID memberPublicId) {
         return expenseRepository.getExpensesByTransactionCategory(memberPublicId);
+    }
+
+    public List<CategoryReport> getMonthlyReport(String username, int month, int year) {
+        Member member = memberRepository.getMemberByUsername(username)
+                .orElseThrow(() -> new NoSuchElementException("Member not found"));
+
+
+        return expenseRepository.getMonthlyExpenses(member, month, year);
     }
 }
