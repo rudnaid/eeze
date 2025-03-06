fb88429f4bb48da073c2a500c3b0dd4d5529da86
Vica1921 <166558654+Vica1921@users.noreply.github.com>
2025-03-06 15:20:09 +0100

    fix(security): add jwt token to showing expenses and incomes in frontend

--- frontend/src/Pages/ExpenseCreator.jsx
+++ frontend/src/Pages/ExpenseCreator.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { redirect, useNavigate } from "react-router-dom";
 import ExpenseForm from "../Components/Forms/ExpenseForm";
 
 const createExpense = (expense) => {
     // change to your own generated database-s no1 members public id!
-
-    return fetch("/api/expenses?memberPublicId=7c873e71-164b-42f8-a6d9-3aa350172c2f", {
+    const token = localStorage.getItem('jwt');
+    return fetch("/api/expenses", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
       },
       body: JSON.stringify(expense),
     }).then((res) => res.json());
   };
 
-const ExpenseCreator = () => {
+const ExpenseCreator = ({onCancel}) => {
     const navigate = useNavigate();
     const [loading, setLoading] = useState(false);
   
@@ -30,7 +31,7 @@ const ExpenseCreator = () => {
   
     return (
       <ExpenseForm
-        onCancel={() => navigate("/")}
+        onCancel={(onCancel)}
         disabled={loading}
         onSave={handleCreateExpense}
       />
