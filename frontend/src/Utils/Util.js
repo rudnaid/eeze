25414732fcb76b5ac147f79c5f6cb56125fcc340
rudnaid <david.rudnai@gmail.com>
2025-03-06 21:45:07 +0100

    feat(util): add getMonthName function to convert int month value to its string equivalent

--- frontend/src/Utils/Util.js
+++ frontend/src/Utils/Util.js
@@ -11,3 +11,7 @@ export const isUserAuthenticated = () => {
 		return false;
 	}
 };
+
+export function getMonthName(monthNum) {
+	return new Intl.DateTimeFormat("en-US", { month: "long" }).format(new Date(2000, monthNum - 1));
+}
