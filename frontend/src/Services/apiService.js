c58ca7fbe69a697de7fc72184d710342eca33a8c
Vica1921 <166558654+Vica1921@users.noreply.github.com>
2025-03-06 10:38:29 +0100

    fix(fetch): reorganize fetches into separate service file

--- frontend/src/Services/apiService.js
+++ frontend/src/Services/apiService.js
@@ -18,4 +18,49 @@ export const fetchMonthlyExpenseReport = async (month, year) => {
   } catch (error) {
     console.error('Error getting monthly expense report:', error);
   }
-}
+};
+
+export const fetchTotalIncomes = async () => {
+  try {
+    const token = localStorage.getItem("jwt");
+    const response = await axios.get("/api/incomes", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error getting all incomes report:', error);
+  }
+};
+
+export const fetchSummary = async () => {
+  try {
+    const token = localStorage.getItem("jwt");
+    const response = await axios.get("/api/reports", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error getting summary:', error);
+  }
+};
+
+export const fetchExpensesByCategory = async () => {
+  try {
+    const token = localStorage.getItem("jwt");
+    const response = await axios.get("/api/reports/by-category", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error getting expenses by category:', error);
+  }
+};
