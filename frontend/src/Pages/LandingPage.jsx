c222e15605983492c9df11a5da205e1a47fcf369
rudnaid <david.rudnai@gmail.com>
2025-03-06 17:12:03 +0100

    feat(landingPage): implement custom hooks with derived state for loading and error, add BarChartComponent and PieChartComponent

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -1,19 +1,40 @@
 import PieChartComponent from "../Components/Charts/PieChartComponent.jsx";
 import Loading from "../Components/Loading/Loading.jsx";
-import {useFetchMonthlyExpenseReport} from "../Hooks/useFetchMonthlyExpenseReport.jsx";
 import ErrorComponent from "../Components/Util/ErrorComponent.jsx";
+import BarChartComponent from "../Components/Charts/BarCharComponent.jsx";
+import {useFetchMonthlyExpenseReport} from "../Hooks/useFetchMonthlyExpenseReport.jsx";
+import useFetchYearlyIncomeExpenseReport from "../Hooks/useFetchYearlyIncomeExpenseReport.jsx";
 
 const LandingPage = () => {
-  const { loading, error, monthlyExpenseReportData } = useFetchMonthlyExpenseReport();
+
+  const {
+    loading: monthlyLoading,
+    error: monthlyError,
+    monthlyExpenseReportData,
+  } = useFetchMonthlyExpenseReport();
+
+  const {
+    loading: yearlyLoading,
+    error: yearlyError,
+    yearlyIncomeExpenseReportData,
+  } = useFetchYearlyIncomeExpenseReport();
+
+  const loading = monthlyLoading || yearlyLoading;
+  const error = monthlyError || yearlyError;
 
   if (loading) return <Loading />;
   if (error) return <ErrorComponent message={error} />;
 
   return (
     <>
-      <PieChartComponent chartData={monthlyExpenseReportData}/>
+      <div>
+        <BarChartComponent chartData={yearlyIncomeExpenseReportData} />
+      </div>
+      <div>
+      <PieChartComponent chartData={monthlyExpenseReportData} />
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default LandingPage;
