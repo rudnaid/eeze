e46e53540c906f1c62de692e94cde72d9a814878
Vica1921 <166558654+Vica1921@users.noreply.github.com>
2025-03-06 16:31:40 +0100

    fix(main): set LandingPage as /home path's element

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,6 +12,7 @@ import Login from './Pages/Login.jsx';
 import Register from './Pages/Register.jsx';
 import PublicLayout from './Layouts/PublicLayout.jsx';
 import AuthenticatedLayout from './Layouts/AuthenticatedLayout.jsx';
+import LandingPage from './Pages/LandingPage.jsx';
 
 const router = createBrowserRouter([
 	{
@@ -39,7 +40,7 @@ const router = createBrowserRouter([
 		children: [
 			{
 				index: true,
-				element: <Expenses />,
+				element: <LandingPage />,
 			},
 			{
 				path: 'expenses',
