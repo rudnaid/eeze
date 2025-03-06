719a6a6b61975f0e626eccedd4004d155044cd1d
rudnaid <david.rudnai@gmail.com>
2025-03-06 13:31:24 +0100

    refactor(dto): change MonthlyIncomeExpenseReportDTO to a class and add lombok annotations

--- backend/src/main/java/com/codecool/spendeeze/model/dto/reports/MonthlyIncomeExpenseReportDTO.java
+++ backend/src/main/java/com/codecool/spendeeze/model/dto/reports/MonthlyIncomeExpenseReportDTO.java
@@ -1,12 +1,3 @@
 package com.codecool.spendeeze.model.dto.reports;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class MonthlyIncomeExpenseReportDTO {
-    private String month;
-    private double totalIncome;
-    private double totalExpense;
-}
+public record MonthlyIncomeExpenseReportDTO(String month, double totalIncome, double totalExpense) {}
