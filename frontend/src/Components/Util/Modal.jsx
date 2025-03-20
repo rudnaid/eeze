971d9dd848c3cabcd8e70c9df00734327c55c11a
rudnaid <david.rudnai@gmail.com>
2025-03-20 10:21:44 +0100

    refactor(modal): format and remove unnecessary imports

--- frontend/src/Components/Util/Modal.jsx
+++ frontend/src/Components/Util/Modal.jsx
@@ -1,29 +1,28 @@
-import { useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import {useEffect, useRef} from "react";
 
-const Modal = ({ openModal, closeModal, children, redirectTo }) => {
-    const ref = useRef();
-    const navigate = useNavigate();
+const Modal = ({openModal, closeModal, children,}) => {
+  const ref = useRef();
 
-    useEffect(() => {
-        if (openModal && ref.current) {
-            ref.current?.showModal();
-        } else if (ref.current) {
-            ref.current?.close();
-        }
-    }, [openModal]);
+  useEffect(() => {
+    if (openModal && ref.current) {
+      ref.current?.showModal();
+    } else if (ref.current) {
+      ref.current?.close();
+    }
+  }, [openModal]);
 
-    const handleClose = () => {
-        closeModal();
-    };
+  const handleClose = () => {
+    closeModal();
+  };
 
-    return ( 
-        <dialog ref={ref} onCancel={handleClose} className="absolute inset-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-transparent p-6 rounded-lg w-120 overflow-y-hidden">
-            <div className="bg-white p-6 rounded-lg shadow-lg w-96">
-                {children(handleClose)}
-            </div>
-        </dialog>
-     );
+  return (
+    <dialog ref={ref} onCancel={handleClose}
+            className="absolute inset-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-transparent p-6 rounded-lg w-120 overflow-y-hidden">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-96">
+        {children(handleClose)}
+      </div>
+    </dialog>
+  );
 }
- 
+
 export default Modal;
